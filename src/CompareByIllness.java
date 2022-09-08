4feb2a917f008a4509bba3efbc46be0a928cce23
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-09-08 18:19:54 +0300

    Реализуем CompareByIllness Не рабочая

--- src/CompareByIllness.java
+++ src/CompareByIllness.java
@@ -1,2 +1,8 @@
-public class CompareByIllness {
+import java.util.Comparator;
+
+public class CompareByIllness implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        return o1.isIllness() == false ? o1.getAge() : o2.getAge();
+    }
 }
